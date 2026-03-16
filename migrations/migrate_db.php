3ed15e4355c17b456fe975bd95597e71c82b2ca7
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-17 02:28:25 +0800

    Started working with the db migrations

--- migrations/migrate_db.php
+++ migrations/migrate_db.php
@@ -1,4 +1,41 @@
 <?php
 
-require_once '../includes/database.inc.php';
+require_once '../includes/dbconfig.php';
 
+/* Create the database */
+
+echo "Preparing to connect to MySQL Server <br>";
+
+try {
+
+    $dsn = "mysql:host=" . DB_HOST;
+
+    $pdo = new PDO($dsn, DB_USER, DB_PASS);
+    echo "Database connection established.<br>";
+} catch (PDOException $e) {
+    throw new PDOException("Error connection to the database server.<br>" . $e->getMessage());
+}
+
+echo "Creating the database<br>";
+
+
+$query = "CREATE DATABASE IF NOT EXISTS `" . DB_NAME . "` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;";
+//execute the query
+try {
+    $pdo->exec($query);
+    echo "Database created <br>";
+} catch (PDOException $e) {
+    throw new PDOException("Error creating the database.<br>" . $e->getMessage());
+}
+
+/* Connect to the database */
+try {
+    $query = "USE `" . DB_NAME . "`";
+    $pdo->exec($query);
+    echo "Successfully selected the database <br>";
+} catch (PDOException $e) {
+    throw new PDOException("Error selecting the database.<br>" . $e->getMessage());
+}
+
+
+/* Create the tables */

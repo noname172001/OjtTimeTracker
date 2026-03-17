f383c4363d1746b4c72533181b073e9905f66082
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-17 20:11:05 +0800

    Change the paswword details of the DB_PASS

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'db');
 define('DB_NAME', 'timetrackerdb');
 define('DB_USER', 'root');
-define('DB_PASS', 'test123');
+define('DB_PASS', '1234');
 define('DB_CHARSET', 'utf8mb4');
 
 // Application Settings
